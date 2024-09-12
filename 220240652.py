b92bb66b2834174a3475884689ab33b8a9da8011
jua05 <sjua1306@gmail.com>
2024-09-12 17:26:40 +0900

    commit_massage_test

---
+++ 220240652.py
@@ -0,0 +1,20 @@
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __mul__(self, num):
+        return Point(self.x*num,self.y*num)
+    def __rmul__(self, num):
+        return Point(self.x*num, self.y*num)
+    def __str__(self):
+        return f"({self.x},{self.y})"
+pt1 = Point(1,2)
+pt2 = Point(3,4)
+pt3 = pt1*10 #x,y 각각 10을 곱함 => pt1.__mul__(10)
+pt4 = -10 * pt2
+
+print(pt1)
+print(pt2)
+print(pt3)
+print(pt4)
