6a603794bfebb0893b990ce6a0e3b47861dcdf17
jua05 <sjua1306@gmail.com>
2024-09-12 17:43:22 +0900

    Update 220240652.py

--- 220240652.py
+++ 220240652.py
@@ -11,12 +11,19 @@ class Point:
 
     def __str__(self):
         return f"({self.x},{self.y})"
+    def ICD(self):
+        a = self.x + self.y
+        return a
 
 pt1 = Point(1,2)
 pt2 = Point(3,4)
 pt3 = pt1*10 #x,y 각각 10을 곱함 => pt1.__mul__(10)
 pt4 = -10 * pt2
 pt5 = 333+111
+pt6 = 2 * pt1
+print(pt6)
+pt7 = Point.ICD(pt1)
+print(pt7)
 
 print(pt1)
 print(pt2)
