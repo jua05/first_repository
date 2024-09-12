b6f788db398a4e2f0e83ad490fbe1a6345beb4f7
jua05 <sjua1306@gmail.com>
2024-09-12 17:33:50 +0900

    Update 220240652.py

--- 220240652.py
+++ 220240652.py
@@ -5,14 +5,18 @@ class Point:
 
     def __mul__(self, num):
         return Point(self.x*num,self.y*num)
+
     def __rmul__(self, num):
         return Point(self.x*num, self.y*num)
+
     def __str__(self):
         return f"({self.x},{self.y})"
+
 pt1 = Point(1,2)
 pt2 = Point(3,4)
 pt3 = pt1*10 #x,y 각각 10을 곱함 => pt1.__mul__(10)
 pt4 = -10 * pt2
+pt5 = 333+111
 
 print(pt1)
 print(pt2)
